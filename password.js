9eb4596c0be499390da8fea34da5eb4afc3d82ef
Walkerheberttgit config --global user.email walkerhebert@yahoo.comgit config --global user.email walkerhebert@yahoo.com <walkerhebert@yahoo.com>
2022-08-19 18:08:12 -0500

    Assessment1

--- password.js
+++ password.js
@@ -1,15 +1,50 @@
-let password = 'Doglover123'
-let number = 1
-let letter = 1
+// let password = 'Doglover123'
+// let number = 1
+// let letter = 1
 
-if(password.length >= 10 && password.length <= 20){
-    console.log('success')
-}else{
-    console.log('failure')
+// if(password.length >= 10 && password.length <= 20){
+//     console.log('success')
+// }else{
+//     console.log('failure')
+// }
+
+// if(number.length >= 0){
+//     console.log('success')
+// }else{
+//     console.log('failure')
+// }
+
+
+let password ='Doglover123'
+
+
+let minChars = false
+let upperCheck = false
+let numCheck = false
+let letterCheck = false
+
+
+for(let i = 0; i < password.length; i++){
+    if(!isNaN(+password[i])){
+        numCheck = true
+    }
+    
+    if(password[i] === password[i].toUpperCase() && isNaN(+password[i])){
+        upperCheck = true
+    }
 }
 
-if(number.length >= 0){
-    console.log('success')
-}else{
-    console.log('failure')
+if(/[a-zA-Z].test(password)/){
+    letterCheck = true
 }
+
+if(password.length >= 10 && password.length <=20){
+    minChars = true
+}
+
+if(minChars && letterCheck && upperCheck && numCheck){
+    console.log('your password fills all the requirements')
+}else {
+    console.log('the password does not fill all the requirements')
+}
+
